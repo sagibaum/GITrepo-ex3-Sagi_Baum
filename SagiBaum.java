8345f8b052e16206e40e3993eaabc64fedc4b9cb
Sagi Baum <sagibaum@gmail.com>
2023-04-20 18:36:44 +0300

    Update SagiBaum.java

--- SagiBaum.java
+++ SagiBaum.java
@@ -1,6 +1,8 @@
 public class SagiBaum {
     public static void main(String[] args) {
+        System.out.println("Sagi Baum job Started ...");
         for (int i = 0; i < 10; i++)
-            System.out.println("Sagi Baum itzik zino");
+            System.out.println(i + "\n");
     }
+    system.out.println("Sagi Baum job Finished!");
 }
